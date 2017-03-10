735a66d9df626d85881c8cc4cf04c4a5ec913e3d
rhys-saldanha <rhys.saldanha@gmail.com>
2017-03-10 15:52:56 +0000

    Make working machineLang
    with type checking

--- machineLang.ml
+++ machineLang.ml
@@ -7,20 +7,22 @@ exception NonBaseTypeResult;;
 
 open Printf;;
 
+(* Types for opperations *)
+type machOpp = MachUnion | MachPrefix | MachInsec
+
 (*Types of machineLang *)
-type machType = machInt | machWord | machFunc of machType * machType
+type machType = MachInt | MachWord | MachLang | MachFunc of machType * machType
 
 (* Grammar of machineLang *)
-type machTerm =
-	MtWord of string
+type machTerm = MtWord of string
+	| MtLang of string
 	| MtNum of int
-	| MtPrefix of machTerm * machTerm
-	| MtInsec of machTerm * machTerm
-	| MtUnion of machTerm * machTerm
+	| MtOpp of machTerm * machTerm * machOpp
 
 let rec isValue e = match e with
 	| MtNum n -> true
 	| MtWord w -> true
+	| MtLang l -> true
 	| _ -> false
 ;;
 
@@ -32,7 +34,7 @@ type termContext = machTerm context
 let rec lookup env str = match env with
 	Env [] -> raise LookupError
 	| Env ((name, item) :: gs) ->
-		(match (name = str) with
+		( match (name = str) with
 			true -> item
 			| false -> lookup (Env (gs)) str
 		)
@@ -41,4 +43,15 @@ let rec lookup env str = match env with
 (* Function to add entry to enviroment *)
 let rec typeOf env e = match e with
 	MtNum n -> MachInt
-	| 
+	| MtWord w -> MachWord
+	| MtLang l -> MachLang
+	| MtOpp (a, b, x) ->
+		( match (typeOf env a) , (typeOf env b) , x with
+			MachWord, MachLang, MachPrefix -> MachLang
+			|	MachLang, MachLang, MachUnion -> MachLang
+			|	MachLang, MachLang, MachInsec -> MachLang
+			| _ -> raise TypeError
+		)
+;;
+
+let typeProg e = typeOf (Env []) e ;;
