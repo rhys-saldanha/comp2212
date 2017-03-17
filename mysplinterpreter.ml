43e0d617223aaee85b71a8a634f3e871f8376551
rhys-saldanha <rhys.saldanha@gmail.com>
2017-03-17 21:25:26 +0000

    Changed input method
    reads file

--- mysplinterpreter.ml
+++ mysplinterpreter.ml
@@ -4,14 +4,14 @@ open Parser
 open Arg
 open Printf
 
-let parseProgram c = 
+let parseProgram c =
     try 
-		let lexbuf = Lexing.from_channel c in  
-            parser_main lexer_main lexbuf 
+		let lexbuf = Lexing.from_string c in
+            parser_main lexer_main lexbuf
     with Parsing.Parse_error -> failwith "Parse failure!"
 ;;
 
-try while true do
+(*try while true do
 		let arg = ref stdin in
 		let setProg p = arg := open_in p in
 		let usage = "./main PROGRAM_FILE" in
@@ -24,4 +24,18 @@ try while true do
 		let () = print_string "Program Evaluated using big step semantics to ==> " ;  print_res result3 ; print_newline() in
 			flush stdout
 	done
-with Lexer.Eof -> exit 0
+with Lexer.Eof -> exit 0*)
+
+let _ =
+	try
+		let file = open_in Sys.argv.(1) in
+		while true do
+			let line = input_line file in
+			let parsed = parseProgram line in
+			let _ = typeProg parsed	in
+			print_string "Program Type Checked\n";
+			let result3 = bigEval parsed in
+			let () = print_string "Program Evaluated using big step semantics to ==> " ;  print_res result3 ; print_newline() in
+				flush stdout
+		done
+	with End_of_file -> exit 0
